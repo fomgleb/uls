da8a76d1496426d65fd472bb7a35626f956ccaf8
fomgleb <fomglib@gmail.com>
2023-03-18 05:09:16 -0700

    Polishing args.c and args.h

--- inc/args.h
+++ inc/args.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "base_includes.h"
-#include "flags.h"
 
 typedef struct s_args {
     t_list *entry_names_list;
@@ -10,6 +9,4 @@ typedef struct s_args {
 
 t_args mx_create_args(void);
 void mx_free_args(t_args args); 
-
 t_args mx_convert_to_args(int argc, char **argv);
-
--- src/args.c
+++ src/args.c
@@ -1,9 +1,10 @@
 #include "../inc/args.h"
 
 t_args mx_create_args(void) {
-    t_args new_args;
-    new_args.entry_names_list = NULL;
-    new_args.flags_str = NULL;
+    t_args new_args = {
+        .entry_names_list = NULL,
+        .flags_str = NULL
+    };
     return new_args;
 }
 
