c1cd8c4548fdf2cf9a63079754908a06c42a5de3
fomgleb <fomglib@gmail.com>
2023-03-25 04:45:18 -0700

    Optimize memory managing

--- src/uls/main.c
+++ src/uls/main.c
@@ -35,27 +35,27 @@ static int prepare_args(t_args *args, const char *existing_args) {
     return args_error.error_code ? 1 : 0;
 }
 
-static t_list *find_entries_list(t_list *input_entry_names_list, t_flags flags) {
+static t_list *find_entries_list(t_list *input_entry_names_list, t_flags *flags) {
     t_list *entries_list = NULL;
 
     for (t_list *node = input_entry_names_list; node != NULL; node = node->next) {
         mx_push_front(&entries_list, mx_create_entry_ptr(node->data));
     }
 
-    if (flags.d) {
+    if (flags->d) {
         return entries_list;
-    } else if (flags.R) {
+    } else if (flags->R) {
         for (t_list *i = entries_list; i != NULL; i = i->next) {
             t_entry *entry = (t_entry *)i->data;
             if (S_ISDIR(entry->stat.st_mode)) {
-                entry->entries_list = mx_get_entries_in_directory_recursively(*entry, flags.a || flags.A, !flags.a && flags.A);
+                entry->entries_list = mx_get_entries_in_directory_recursively(*entry, flags->a || flags->A, !flags->a && flags->A);
             }
         }
     } else {
         for (t_list *node = entries_list; node != NULL; node = node->next) {
             t_entry *entry = (t_entry *)node->data;
             if (S_ISDIR(entry->stat.st_mode)) {
-                entry->entries_list = mx_get_entries_in_directory(*entry, flags.a || flags.A, !flags.a && flags.A);
+                entry->entries_list = mx_get_entries_in_directory(*entry, flags->a || flags->A, !flags->a && flags->A);
             }
         }
     }
@@ -63,15 +63,15 @@ static t_list *find_entries_list(t_list *input_entry_names_list, t_flags flags) 
     return entries_list;
 }
 
-static void mx_sort_entries_list_recursively(t_list *entries_list, t_flags flags) {
-    if (flags.f) {
+static void mx_sort_entries_list_recursively(t_list *entries_list, t_flags *flags) {
+    if (flags->f) {
         return;
-    } else if (flags.S) {
+    } else if (flags->S) {
         mx_sort_list(entries_list, mx_sort_entries_by_size);
-    } else if (flags.t) {
-        if (flags.u > flags.U && flags.u > flags.c) {
+    } else if (flags->t) {
+        if (flags->u > flags->U && flags->u > flags->c) {
             mx_sort_list(entries_list, mx_sort_entries_by_last_access);
-        } else if (flags.U > flags.u && flags.U > flags.c) {
+        } else if (flags->U > flags->u && flags->U > flags->c) {
             mx_sort_list(entries_list, mx_sort_entries_by_creation_time);
         } else {
             mx_sort_list(entries_list, mx_sort_entries_by_last_modification);
@@ -80,7 +80,7 @@ static void mx_sort_entries_list_recursively(t_list *entries_list, t_flags flags
         mx_sort_list(entries_list, mx_sort_entries_by_name);
     }
 
-    if (flags.r) {
+    if (flags->r) {
         mx_reverse_list(entries_list);
     }
 
@@ -96,8 +96,8 @@ int main(c_int argc, c_str_arr argv) {
     t_args args = mx_convert_to_args(argc, (const char **)argv);
     int error_code = prepare_args(&args, EXISTING_FLAGS);
     t_flags flags = mx_create_flags(args.flags_str);
-    t_list *entries_list = find_entries_list(args.entry_names_list, flags);
-    mx_sort_entries_list_recursively(entries_list, flags);
+    t_list *entries_list = find_entries_list(args.entry_names_list, &flags);
+    mx_sort_entries_list_recursively(entries_list, &flags);
     // mx_print_entries(entries_list, get_output_format(flags), get_printing_flags(flags), &flags);
     mx_print_files_and_directories(entries_list, &flags);
 
