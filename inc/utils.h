f75bf86ba0d1c66e993cb1ad263263aff5ab3462
fomgleb <fomglib@gmail.com>
2023-03-30 10:14:35 -0700

    Add two printing functions
    mx_print_char_and_int(char, int)
    mx_print_char_and_int_if(bool, char, int)

--- inc/utils.h
+++ inc/utils.h
@@ -87,6 +87,8 @@ float mx_round_down(c_float number);
 float mx_round_up(c_float number);
 float mx_round(c_float number);
 
+void mx_print_char_and_int_if( c_bool condition, c_char character, c_int integer);
+void mx_print_char_and_int(c_char character, c_int integer);
 void mx_print_char_and_string(c_char character, c_str string);
 void mx_print_two_chars(c_char character1, c_char character2);
 void mx_print_two_strings_if(c_bool condition, c_str string1, c_str string2);
