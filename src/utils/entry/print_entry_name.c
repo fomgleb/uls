782fbd77563261ea691b6c304fca4e3e7d218ab4
fomgleb <fomglib@gmail.com>
2023-04-04 01:41:43 -0700

    Print green filename if it is a socket

--- src/utils/entry/print_entry_name.c
+++ src/utils/entry/print_entry_name.c
@@ -9,6 +9,8 @@ size_t mx_print_entry_name(t_entry *entry, bool colorized, bool slash_after_dirs
             mx_printstr("\x1b[35m");
         } else if (mode & S_IXUSR || mode & S_IXGRP || mode & S_IXOTH) {
             mx_printstr("\x1b[31m");
+        } else if (S_ISSOCK(mode)) {
+            mx_printstr("\x1b[32m");
         }
     }
     size_t size_of_printed = mx_printstr(entry->dirent ? entry->dirent->d_name : entry->relative_path);
